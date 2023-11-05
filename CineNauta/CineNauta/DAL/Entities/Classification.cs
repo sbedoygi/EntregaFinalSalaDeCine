4cc97821e4dc3a9a7707b11d3e586e5ca32ceebb
SEBASTIAN BEDOYA GIL <sbedoygi@gmail.com>
2023-11-05 11:18:03 -0500

    An identity is created for the database, classification, and gender

---
+++ CineNauta/CineNauta/DAL/Entities/Classification.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Cine_Nauta.DAL.Entities
+{
+    public class Classification : EntityCine
+    {
+        [Display(Name = "Clasificación")]
+        [MaxLength(50)]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string ClassificationName { get; set; }
+
+        [Display(Name = "Descripción")]
+        [MaxLength(400)]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Description { get; set; }
+
+      
+    }
+}
---
+++ CineNauta/CineNauta/DAL/Entities/Gender.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Cine_Nauta.DAL.Entities
+{
+    public class Gender : EntityCine
+    {
+        [Display(Name = "Genero")]
+        [MaxLength(50)]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string GenderName { get; set; }
+
+        [Display(Name = "Descripción")]
+        [MaxLength(200)]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Description { get; set; }
+
+    }
+}
