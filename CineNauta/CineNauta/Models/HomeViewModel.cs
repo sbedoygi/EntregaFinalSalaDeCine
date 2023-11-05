11a1e28b07cdaa000501c9b2a5a1f878c1c57633
Sebastian Londoño <Valenciasebastian80@gmail.com>
2023-11-05 17:49:09 -0500

    created class MovieDetailsViewModelAndEditHome

---
+++ CineNauta/CineNauta/Models/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Cine_Nauta.DAL.Entities;
+
+namespace Cine_Nauta.Models
+{
+    public class HomeViewModel
+    {
+        public ICollection<Movie> Movies { get; set; }
+
+
+
+    
+    }
+}
