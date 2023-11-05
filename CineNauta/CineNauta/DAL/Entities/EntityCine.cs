38424cc5e0e5bf99ce8ea145651e57baabf0d35c
SEBASTIAN BEDOYA GIL <sbedoygi@gmail.com>
2023-11-05 11:10:30 -0500

    Created Entities Cine and Room for database

---
+++ CineNauta/CineNauta/DAL/Entities/EntityCine.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Cine_Nauta.DAL.Entities
+{
+    public class EntityCine
+    {
+        [Key]
+        public virtual int Id { get; set; }
+
+        [Display(Name = "Fecha de creación")]
+        public virtual DateTime? CreatedDate { get; set; }
+
+        [Display(Name = "Fecha de modificación")]
+        public virtual DateTime? ModifiedDate { get; set; }
+    }
+}
---
+++ CineNauta/CineNauta/DAL/Entities/Room.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Cine_Nauta.DAL.Entities
+{
+    public class Room : EntityCine
+    {
+        [Display(Name = "Numero de Sala")]
+        [MaxLength(10, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string NumberRoom { get; set; }
+
+        [Display(Name = "Capacidad")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public int Capacity { get; set; }
+
+        [Display(Name = "Tipo de Sala")]
+        [MaxLength(10, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string TypeRoom { get; set; }
+    }
+}
