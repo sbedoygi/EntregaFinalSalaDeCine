29db30a5c35ab81aa1127df2c080abc6b7160346
Sebastian Londoño <Valenciasebastian80@gmail.com>
2023-11-05 16:23:43 -0500

    create classes EditFunctionViewModelDetails AndEdit : EntityCine

---
+++ CineNauta/CineNauta/Models/AddMovieViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Cine_Nauta.DAL.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Cine_Nauta.Models
+{
+    public class AddMovieViewModel : EditMovieViewModel
+    {
+
+        [Display(Name = "Titulo")]
+        [MaxLength(80, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Title { get; set; }
+
+        [Display(Name = "Descripción")]
+        [MaxLength(400, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Description { get; set; }
+
+        [Display(Name = "Director")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Director { get; set; }
+
+        [Display(Name = "Duracion")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public int Duration { get; set; }
+
+        [Display(Name = "Genero")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public int GenderId { get; set; }
+        public IEnumerable<SelectListItem> Genders { get; set; }
+
+        [Display(Name = "Clasificacion")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public int ClassificationId { get; set; }
+        public IEnumerable<SelectListItem> Classifications { get; set; }
+    }
+}
---
+++ CineNauta/CineNauta/Models/EditFunctionViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Cine_Nauta.DAL.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Cine_Nauta.Models
+{
+    public class EditFunctionViewModel : EntityCine
+    {
+        [Display(Name = "Fecha Función")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public DateTime FunctionDate { get; set; }
+
+        [Display(Name = "Precio")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public decimal Price { get; set; }
+
+
+        [Display(Name = "Pelicula")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public int MovieId { get; set; }
+
+
+        [Display(Name = "Sala")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public int RoomId { get; set; }
+        public IEnumerable<SelectListItem> Rooms { get; set; }
+        public IEnumerable<SelectListItem> Movies { get; internal set; }
+    }
+}
