ec72af5a8cd16b63cde91db1811ec468e82bae86
Sebastian Londoño <Valenciasebastian80@gmail.com>
2023-11-05 14:30:27 -0500

    "A folder named 'helpers' and the IUserHelper interface are created."

---
+++ CineNauta/CineNauta/Helpers/IUserHelper.cs
@@ -0,0 +1,20 @@
+﻿using Cine_Nauta.DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace Cine_Nauta.Helpers
+{
+    public interface IUserHelper
+    {
+        // Firmas para metodos
+        Task<User> GetUserAsync(string email); //captura los usuarios de la base de datos
+
+        Task<IdentityResult> AddUserAsync(User user, string password); //Adiciona usuarios a la base de datos
+        Task AddRoleAsync(string roleName); //Yo tengo los Roles: Admin y User, estos dos roles los va a agregar en la tabla AspNetRoles
+
+        Task AddUserToRoleAsync(User user, string roleName); //Relaciona la tabla User with Roles, agrega un usuario nuevo y le asigna uno de los roles
+
+        Task<bool> IsUserInRoleAsync(User user, string roleName); //Valida si un usuario pertenece a un Rol
+      
+        Task LogoutAsync();
+    }
+}
